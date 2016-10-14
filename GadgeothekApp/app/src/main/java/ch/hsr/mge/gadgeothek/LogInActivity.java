7f4964c044ebbbc1af701b9d57296a6843e1c7f6
Luca Salzani <Alucard1766@users.noreply.github.com>
2016-10-14 12:00:30 +0200

    Done LogIn and Register Layout

--- GadgeothekApp/app/src/main/java/ch/hsr/mge/gadgeothek/LogInActivity.java
+++ GadgeothekApp/app/src/main/java/ch/hsr/mge/gadgeothek/LogInActivity.java
@@ -3,11 +3,12 @@ package ch.hsr.mge.gadgeothek;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LogInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+        setTitle("Log In");
     }
 }
