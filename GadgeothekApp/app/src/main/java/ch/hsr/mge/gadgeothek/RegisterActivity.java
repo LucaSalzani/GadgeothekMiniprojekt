046d6898ac794cffed1580a8c0dbe1d5fba539c6
Luca Salzani <Alucard1766@users.noreply.github.com>
2016-10-14 12:04:36 +0200

    Test Commit
    Fix #2

--- GadgeothekApp/app/src/main/java/ch/hsr/mge/gadgeothek/RegisterActivity.java
+++ GadgeothekApp/app/src/main/java/ch/hsr/mge/gadgeothek/RegisterActivity.java
@@ -9,6 +9,6 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        setTitle("Register");
+        setTitle("New Registration");
     }
 }
