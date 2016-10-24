a942ac562338bec79b2ed163591161f9dfc9c888
gubelli <a>
2016-10-24 15:04:32 +0200

    Update Reservationview

--- GadgeothekApp/app/src/main/java/ch/hsr/mge/gadgeothek/ReservationFragment.java
+++ GadgeothekApp/app/src/main/java/ch/hsr/mge/gadgeothek/ReservationFragment.java
@@ -26,7 +26,7 @@ public class ReservationFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_issue, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_reservation, container, false);
 
         recyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView);
 
